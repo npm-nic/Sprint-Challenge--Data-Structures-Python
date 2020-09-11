660cfc816aef8344ce40316abb707a0d6dc5ad6b
Nic <53277092+npm-nic@users.noreply.github.com>
2020-09-11 12:22:23 -0500

    names (MVP)

--- names/names.py
+++ names/names.py
@@ -12,11 +12,27 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# ----------> o(n^2) ... polynomial <----------
+'''
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
+for name_1 in names_1: # o(n)
+    for name_2 in names_2: # o(n)
         if name_1 == name_2:
-            duplicates.append(name_1)
+            duplicates.append(name_1) # o(c)
+'''
+# ----------> o(n) ... linear <----------
+# create empty {} to hold names
+# FOR: all names in names_1
+    # add to a dictionary
+# FOR: names in name_2
+    # IF: name is already in names {}
+        # add into duplicates (line 13) 
+names = {}
+for name in names_1:
+    names[name] = True
+for name in names_2:
+    if name in names:
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
